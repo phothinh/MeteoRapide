3a887ba842425ba8bca419b2b7c338bc897d90f4
Henri PHOTHINANTHA <phothinh01@gmail.com>
2023-05-15 11:04:02 +0200

    manifest update

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       manifest:{
         "theme_color": "#f69435",
         "background_color": "#f69435",
-        "display": "browser",
+        "display": "standalone",
         "scope": "/",
         "start_url": "/",
         "name": "telephone_arabe",
