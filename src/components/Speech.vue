8eaef8db5062197a8d75ae6a028b7535d3eafe1e
Henri PHOTHINANTHA <phothinh01@gmail.com>
2023-05-15 14:14:06 +0200

    1er test

---
+++ src/components/Speech.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <input v-model="phrase" type="text" placeholder="Entrez une phrase">
+      <button @click="lirePhrase">Lire la phrase</button>
+    </div>
+  </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                phrase: '',
+            };
+        },
+        methods: {
+            lirePhrase() {
+                if ('speechSynthesis' in window) {
+                const utterance = new SpeechSynthesisUtterance(this.phrase);
+                speechSynthesis.speak(utterance);
+                } else {
+                console.log("L'API Speech Synthesis n'est pas prise en charge par ce navigateur.");
+                }
+            },
+            },
+    }
+</script>
